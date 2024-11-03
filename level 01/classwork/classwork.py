7abf9f5d0ffaaeb99fa316beb259aa27d7d82897
sabagabidzashvili <sabagabidzashvili97@gmail.com>
2024-11-03 20:12:12 +0400

    davaleba

---
+++ level 01/classwork/classwork.py
@@ -0,0 +1,4 @@
+name = "saba"
+print(name)
+name = "gabidzashvilu"
+print(name)
---
+++ level 01/homework/homework.py
@@ -0,0 +1,7 @@
+name = "saba"
+surname = "gabidashvili"
+age = 14
+mother = "magda"
+father = "dato"
+number =  "1,23,4,56,8,90"
+print( name , surname , age , mother, father, number )
