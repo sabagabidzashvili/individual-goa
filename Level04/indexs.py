f5323daec6a15a1ca3be235376501c370136b67a
sabagabidzashvili <sabagabidzashvili97@gmail.com>
2024-11-10 22:03:42 +0400

    commit

---
+++ Level04/indexs.py
@@ -0,0 +1,47 @@
+num1=5
+num2=100
+# math + - *, **, /, //
+print(num1 - num2 )
+print(num1 + num2 )
+print(num1 * num2 )
+print(num1 ** num2 )
+print(num1 // num2 )
+print(num1 / num2 )
+num3=9
+num4=1
+print(num3-num4)
+num5=4
+num6=3
+print(num5+num6)
+num7=6
+num9=7
+print(num7 * num9)
+num200=5
+num100=2
+print(num200 ** num100)
+num8=60
+num80=50
+print(num8//num80)
+num30=40
+num20=50
+print(num30 / num20 )
+
+num1 = 1
+num2 = 2
+print(num1 > num2) # False
+print(num1 >= num2) # False
+print(num1 < num2) # True
+print(num1 <= num2) # True
+print(num1 == num2) # False
+print(num1 != num2) # True
+
+num3 = 4
+num4 = 89
+print(num3 > num4) 
+print(num3 >= num4) 
+print(num3 < num4) 
+print(num3 <= num4) 
+print(num3 == num4) 
+print(num4 != num4) 
+
+
