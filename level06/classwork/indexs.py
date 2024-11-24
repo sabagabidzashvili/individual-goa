ede7403731c2323ded7679b205c53fcfd6b58045
sabagabidzashvili <sabagabidzashvili97@gmail.com>
2024-11-24 20:32:29 +0400

    commit

---
+++ level06/classwork/indexs.py
@@ -0,0 +1,10 @@
+print(True or False)
+print(False or True)
+print(True or False) 
+print(True and True)
+print(False and False)
+print(True and False)
+if 10>4:
+    print("თუ ეს სწორია პასუხი გამომიტანე")
+elif 10<4:
+    print("რომელია მარტალი მიშა თუ გრიშა")
